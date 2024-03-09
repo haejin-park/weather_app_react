f0b7f8e70dff7868a81b20a80167056e8efbb761
haejin <luckyhaejin1126@gmail.com>
2024-03-09 17:59:43 +0900

    주석 삭제

--- src/App.js
+++ src/App.js
@@ -3,14 +3,6 @@ import './App.css';
 import WeatherBox from './component/WeatherBox';
 import WeatherButton from './component/WeatherButton'
 import { ClipLoader } from 'react-spinners';
-/*
-1. 앱이 실행되자마자 현재 위치 기반의 날씨가 보인다.
-2. 날씨 정보에는 도시, 섭씨, 화씨 날씨상태
-3. 5개의 버튼이 있다.(1개는 현재위치, 4개는 다른 도시)
-4. 도시 버튼을 클릭할 때 마다 도시별 날씨가 나온다.
-5. 현재 위치 버튼을 누르면 다시 현재 위치 기반의 날씨가 나온다. 
-6. 데이터를 들고오는 동안 로딩 스피너가 돈다.
-*/ 
 
 function App() {
 
